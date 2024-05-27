a8be2b2ea9d458d496f2bdcb072f3d31646642a1
Emanuel Silva <manusilvaribeiro10@gmail.com>
2024-05-27 11:30:25 -0300

    Erro de parsing

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -15,27 +15,23 @@ describe('Register Use Case', () => {
       password: '123456',
     });
 
-    expect(user.id).toEqual(expect.any(String))
+    expect(user.id).toEqual(expect.any(String));
   });
 });
 
-  it('should hash user password upon registration', async () => {
-    const UsersRepository = new InMemoryUsersRepository();
-    const registerUseCase = new RegisterUseCase(UsersRepository);
+it('should hash user password upon registration', async () => {
+  const UsersRepository = new InMemoryUsersRepository();
+  const registerUseCase = new RegisterUseCase(UsersRepository);
 
-    const { user } = await registerUseCase.execute({
-      name: 'John Doe',
-      email: 'johndoe@example.com',
-      password: '123456',
-    });
-
-    const isPasswordCorrectlyHashed = await compare(
-      '123456',
-      user.password_hash,
-    );
-
-    expect(isPasswordCorrectlyHashed).toBe(true);
+  const { user } = await registerUseCase.execute({
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password: '123456',
   });
+
+  const isPasswordCorrectlyHashed = await compare('123456', user.password_hash);
+
+  expect(isPasswordCorrectlyHashed).toBe(true);
 });
 
 it('should not be able to register with the same email twice', async () => {
