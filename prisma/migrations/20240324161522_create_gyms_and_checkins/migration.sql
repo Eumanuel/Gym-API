f6bcac068a59f24c1f143606c2bbc27a360fbbd0
Emanuel Silva <manusilvaribeiro10@gmail.com>
2024-06-24 17:56:08 -0300

    Fix name of field

--- prisma/migrations/20240324161522_create_gyms_and_checkins/migration.sql
+++ prisma/migrations/20240324161522_create_gyms_and_checkins/migration.sql
@@ -24,7 +24,7 @@ CREATE TABLE "gyms" (
     "description" TEXT,
     "phone" TEXT,
     "latitude" DECIMAL(65,30) NOT NULL,
-    "longidude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
 
     CONSTRAINT "gyms_pkey" PRIMARY KEY ("id")
 );
